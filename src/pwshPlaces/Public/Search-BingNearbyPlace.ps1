fb07b207130f02a6c78d4ad7c782bb01430b0a01
Jake Morrison <tech@techthoughts.info>
2023-12-16 22:50:41 -0600

    updated help for Search-BingNearbyPlace

--- src/pwshPlaces/Public/Search-BingNearbyPlace.ps1
+++ src/pwshPlaces/Public/Search-BingNearbyPlace.ps1
@@ -1,35 +1,36 @@
 ﻿<#
 .SYNOPSIS
-    Nearby Search lets you search for different place types within a specified area.
+    Nearby Search lets you search for places of interest within a specified area using Bing Maps API.
 .DESCRIPTION
-    Performs a nearby search request with provided parameters.
-    Nearby search is useful for finding place types near a specific geographic location.
-    By default the location bias is IP based.
-    Location bias can be controlled via parameters.
+    The Search-BingNearbyPlace function conducts a nearby search for various place types, such as attractions,
+    restaurants, or parks, within a specified geographic area. Users can define the search area using coordinates,
+    radius, or a rectangular area. By default, search results are biased based on the user's IP location,
+    but this can be customized using various geographic parameters.
+    Location bias and language can also be controlled via parameters.
 .EXAMPLE
     Search-BingNearbyPlace -Type Attractions -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search biased by IP and returns Attraction places types.
+    Searches for attractions near the user's IP location.
 .EXAMPLE
     Search-BingNearbyPlace -Type Restaurants -MaxResults 20 -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search biased by IP and returns Restaurant places types with a maximum of 20 results returned.
+    Finds up to 20 restaurants near the user's IP location.
 .EXAMPLE
     Search-BingNearbyPlace -Type CafeRestaurants -PointLatitude '29.7049806' -PointLongitude '-98.068343' -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search near the provided Lat/Long and returns CafeRestaurant places types.
+    Searches for cafe restaurants near the specified coordinates.
 .EXAMPLE
     Search-BingNearbyPlace -Type BreweriesAndBrewPubs -CircleLatitude '29.7049806' -CircleLongitude '-98.068343' -CircleRadius '5000' -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search biased by circle lat/long/radius and returns Bars and Pubs places types.
+    Searches for breweries and brewpubs within a 5000-meter radius of the specified circle coordinates.
 .EXAMPLE
     Search-BingNearbyPlace -Type Parks -SouthLatitude '39.8592387' -WestLongitude '-75.295486' -NorthLatitude '40.0381942' -EastLongitude '-75.0064087' -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search biased by rectangular two lat/lng pairs in decimal degrees, representing the south/west and north/east points of a rectangle. Park places types are returned.
+    Searches for parks within a rectangular area defined by two sets of latitude and longitude coordinates.
 .EXAMPLE
     Search-BingNearbyPlace -Type Museums -PointLatitude '29.7049806' -PointLongitude '-98.068343' -Language en -MaxResults 20 -BingMapsAPIKey $bingAPIKey
 
-    Performs a nearby search near the provided Lat/Long and returns Museum places types. Results are returned in English with a maximum of 20 results returned.
+    Finds up to 20 museums near the specified coordinates, with results in English.
 .EXAMPLE
     $searchBingNearbyPlaceSplat = @{
         Type           = 'Museums'
@@ -41,7 +42,7 @@
     }
     Search-BingNearbyPlace @searchBingNearbyPlaceSplat
 
-    Performs a nearby search near the provided Lat/Long and returns Museum places types. Results are returned in English with a maximum of 20 results returned.
+    Finds up to 20 museums near the specified coordinates, with results in English.
 .PARAMETER Query
     Specifies the search term string, such as an address, business name, or landmark name.
 .PARAMETER PointLatitude
@@ -209,7 +210,7 @@ function Search-BingNearbyPlace {
             $fLocationBias = '&userMapView={0}' -f [uri]::EscapeDataString($combinedRectangle)
             $uri += $fLocationBias
         } #rectangle
-    } #switch_parametersetname
+    } #switch_ParameterSetName
 
     if ($RegionBias) {
         Write-Debug -Message ('RegionBias: {0}' -f $RegionBias)
@@ -245,7 +246,7 @@ function Search-BingNearbyPlace {
     }
 
     if ($results.statusDescription -ne 'OK') {
-        Write-Warning -Message 'Did not get a succcessful return from Bing Location API endpoint'
+        Write-Warning -Message 'Did not get a successful return from Bing Location API endpoint'
         $finalResults = $results
     }
     elseif (-not ($results.resourceSets.estimatedTotal -ge 1)) {
